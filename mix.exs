e5d961c706729c44e433242b168bcc61bde55baf
Will Gilman <will@samadhi.tech>
2017-09-14 12:10:52 -0700

    Updated to Elixir 1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LogCapture.Mixfile do
   def project do
     [app: :log_capture,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
